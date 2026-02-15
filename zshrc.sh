6ffc74ea48a1acb3b24ebe0caa136ff8def5100a
Brook Nash <252729190+nullbrna@users.noreply.github.com>
2026-02-15 23:00:18 +0000

    chore(zshrc): 4-space width

--- zshrc.sh
+++ zshrc.sh
@@ -6,25 +6,25 @@ COL_GIT=229
 COL_MUT=240
 
 function branch() {
-  local detail
+    local detail
 
-  # Get branch-specific detail. Count lines and trim leading whitespace.
-  local working=$(git status --short            2> /dev/null | grep " M\| D\|??" | wc -l | tr -d " ")
-  local staging=$(git diff   --cached --numstat 2> /dev/null                     | wc -l | tr -d " ")
-  local stashed=$(git stash  list               2> /dev/null                     | wc -l | tr -d " ")
+    # Get branch-specific detail. Count lines and trim leading whitespace.
+    local working=$(git status --short            2> /dev/null | grep " M\| D\|??" | wc -l | tr -d " ")
+    local staging=$(git diff   --cached --numstat 2> /dev/null                     | wc -l | tr -d " ")
+    local stashed=$(git stash  list               2> /dev/null                     | wc -l | tr -d " ")
 
-  # Concatenate all details if set.
-  if [[ $working != "0" ]]; then detail+="~$working"; fi
-  if [[ $staging != "0" ]]; then detail+="+$staging"; fi
-  if [[ $stashed != "0" ]]; then detail+="!$stashed"; fi
+    # Concatenate all details if set.
+    if [[ $working != "0" ]]; then detail+="~$working"; fi
+    if [[ $staging != "0" ]]; then detail+="+$staging"; fi
+    if [[ $stashed != "0" ]]; then detail+="!$stashed"; fi
 
-  local name=$(git symbolic-ref --short HEAD 2> /dev/null)
-  if [[ ! -z $name ]]; then echo "%F{$COL_GIT}$name%f%F{$COL_MUT}$detail%f "; fi
+    local name=$(git symbolic-ref --short HEAD 2> /dev/null)
+    if [[ ! -z $name ]]; then echo "%F{$COL_GIT}$name%f%F{$COL_MUT}$detail%f "; fi
 }
 
 function prompt() {
-  local directory="%F{$COL_DIR}%1d%f"
-  PROMPT="$directory $(branch)"
+    local directory="%F{$COL_DIR}%1d%f"
+    PROMPT="$directory $(branch)"
 }
 
 autoload -Uz add-zsh-hook
