acb976caaf81ae0f862e7d205317d3046c273c77
Brook Nash <252729190+nullbrna@users.noreply.github.com>
2026-06-08 21:55:28 +0100

    feat: muted & sectioned prompt

--- zshrc.sh
+++ zshrc.sh
@@ -9,35 +9,52 @@ function stoll() {
     # First row (excluding the title row), first column.
     local model=$(ollama ps | awk 'NR==2 {print $1}')
 
-    [[ -z $model ]] && return
+    [[ -z "$model" ]] && return
     ollama stop "$model" && echo "stopped: $model"
 }
 
 # Prompt
 ##############
 
-DIR_COL=025 BRANCH_COL=229 MUTED_COL=240
+# $1: ANSI colour code.
+# $2: Text to be coloured.
+function as_colour() {
+    echo "%F{$1}$2%f"
+}
+
+# $1: Text to be wrapped in delimiters.
+function in_section() {
+    local delim_col=242 section_col=248
+
+    # Delimiter prefix & suffix. Coloured input text.
+    echo "$(as_colour $delim_col "[")$(as_colour $section_col "$1")$(as_colour $delim_col "]")"
+}
 
 function branch_metadata() {
-    [[ -z $BRANCH ]] && return
+    [[ -z "$BRANCH" ]] && return
 
     # Get branch-specific detail. Count lines and trim leading whitespace.
     local working=$(git status --short            2> /dev/null | grep -c " M\| D\|??")
     local staging=$(git diff   --cached --numstat 2> /dev/null | grep -c "")
     local stashed=$(git stash  list               2> /dev/null | grep -c "")
 
-    local detail
-    # Concatenate all details if set.
-    (( working )) && detail+="~$working"
-    (( staging )) && detail+="+$staging"
-    (( stashed )) && detail+="!$stashed"
+    local working_col=005 staging_col=004 stashed_col=003 detail
 
-    echo "%F{$BRANCH_COL}$BRANCH%f%F{$MUTED_COL}$detail%f "
+    # Concatenate all existing details.
+    (( working )) && detail+="$(as_colour $working_col "~$working")"
+    (( staging )) && detail+="$(as_colour $staging_col "+$staging")"
+    (( stashed )) && detail+="$(as_colour $stashed_col "!$stashed")"
+    # Insert padding prefix if set.
+    [[ -n "$detail" ]] && detail=" $detail"
+
+    echo " $(in_section $BRANCH$detail)"
 }
 
 function update_hook() {
     BRANCH=$(git symbolic-ref --short HEAD 2> /dev/null)
-    PROMPT="%F{$DIR_COL}%1d%f $(branch_metadata)"
+
+    # Overwrite prompt. Trailing space for command input.
+    PROMPT="$(in_section "%1d")$(branch_metadata) "
 }
 
 # Lazily load the hook function.
@@ -51,7 +68,7 @@ bindkey "^[f" history-beginning-search-forward  # opt+left
 bindkey "^[b" history-beginning-search-backward # opt+right
 
 function print_branch() {
-    LBUFFER+=$BRANCH
+    LBUFFER+="$BRANCH"
 }
 
 # Bind the function before assigning the shortcut.
