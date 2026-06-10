26fc235b7fd5459faea0659872f2a993ce1de230
Brook Nash <252729190+nullbrna@users.noreply.github.com>
2026-06-10 19:52:07 +0100

    feat: swapped working & stashed colours

--- zshrc.sh
+++ zshrc.sh
@@ -8,8 +8,8 @@ export EVC_ASYNC_BRA_MAIN="echo 'one', echo 'two'"
 function stoll() {
     # First row (excluding the title row), first column.
     local model=$(ollama ps | awk 'NR==2 {print $1}')
-
     [[ -z "$model" ]] && return
+
     ollama stop "$model" && echo "stopped: $model"
 }
 
@@ -25,8 +25,7 @@ function as_colour() {
 # $1: Text to be wrapped in delimiters.
 function in_section() {
     local delim_col=242 section_col=248
-
-    # Delimiter prefix & suffix. Coloured input text.
+    # Delimiter prefix and suffix. Coloured input text.
     echo "$(as_colour $delim_col "[")$(as_colour $section_col "$1")$(as_colour $delim_col "]")"
 }
 
@@ -38,13 +37,12 @@ function branch_metadata() {
     local staging=$(git diff   --cached --numstat 2> /dev/null | grep -c "")
     local stashed=$(git stash  list               2> /dev/null | grep -c "")
 
-    local working_col=005 staging_col=004 stashed_col=003 detail
+    local working_col=003 staging_col=004 stashed_col=005 detail
 
-    # Concatenate all existing details.
+    # Concatenate all modified, staged, and stashed changes.
     (( working )) && detail+="$(as_colour $working_col "~$working")"
     (( staging )) && detail+="$(as_colour $staging_col "+$staging")"
     (( stashed )) && detail+="$(as_colour $stashed_col "!$stashed")"
-    # Insert padding prefix if set.
     [[ -n "$detail" ]] && detail=" $detail"
 
     echo " $(in_section $BRANCH$detail)"
@@ -52,12 +50,10 @@ function branch_metadata() {
 
 function update_hook() {
     BRANCH=$(git symbolic-ref --short HEAD 2> /dev/null)
-
     # Overwrite prompt. Trailing space for command input.
     PROMPT="$(in_section "%1d")$(branch_metadata) "
 }
 
-# Lazily load the hook function.
 autoload -Uz add-zsh-hook && add-zsh-hook precmd update_hook
 
 # Keybindings
@@ -71,7 +67,6 @@ function print_branch() {
     LBUFFER+="$BRANCH"
 }
 
-# Bind the function before assigning the shortcut.
 zle -N print_branch && bindkey "^B" print_branch # ctrl+b
 
 # Dependencies
