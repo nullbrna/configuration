dfae9966b7f741311f4b55a48423617f24d328de
Brook Nash <252729190+nullbrna@users.noreply.github.com>
2026-07-04 17:57:02 +0100

    feat(zshrc): delete unused environment variables

--- zshrc.sh
+++ zshrc.sh
@@ -1,12 +1,9 @@
 # Environment
 ##############
 
-export EVC_DIR_ENVCMD="echo 'foo', echo 'bar'"
-export EVC_ASYNC_BRA_MAIN="echo 'one', echo 'two'"
-
-# Stop a single "ollama" model instance. Only stops the top-most model listed.
+# Stop a single Ollama model instance. Only stops the top-most model listed.
 function stoll() {
-    # First row, excluding the title row, first column.
+    # First row, excluding the title row, and first column.
     local model=$(ollama ps | awk 'NR==2 {print $1}')
     [[ -z "$model" ]] && return
 
