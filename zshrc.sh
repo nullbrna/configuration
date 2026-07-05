82c96e662f23f50d5a1037c2c04551cfdc7f09cd
Brook Nash <252729190+nullbrna@users.noreply.github.com>
2026-07-05 18:45:58 +0100

    feat(zshrc): source for Orbstack

--- zshrc.sh
+++ zshrc.sh
@@ -1,7 +1,6 @@
 # Environment
 ##############
 
-# Stop a single Ollama model instance. Only stops the top-most model listed.
 function stoll() {
     # First row, excluding the title row, and first column.
     local model=$(ollama ps | awk 'NR==2 {print $1}')
@@ -19,36 +18,33 @@ function as_colour() {
     echo "%F{$1}$2%f"
 }
 
-# $1: Text to be wrapped in delimiters.
-function in_section() {
+# $1: Text to be wrapped in a delimiter.
+function wrap_in_section() {
     local delim_col=242 section_col=248
-    # Delimiter prefix and suffix. Coloured input text.
     echo "$(as_colour $delim_col "[")$(as_colour $section_col "$1")$(as_colour $delim_col "]")"
 }
 
 function branch_metadata() {
     [[ -z "$BRANCH" ]] && return
 
-    # Get branch-specific detail. Count lines and trim leading whitespace.
+    # Get branch detail. Count lines and trim leading whitespace.
     local working=$(git status --short            2> /dev/null | grep -c " M\| D\|??")
     local staging=$(git diff   --cached --numstat 2> /dev/null | grep -c "")
     local stashed=$(git stash  list               2> /dev/null | grep -c "")
 
     local working_col=003 staging_col=004 stashed_col=005 detail
 
-    # Concatenate all modified, staged, and stashed changes.
     (( working )) && detail+="$(as_colour $working_col "~$working")"
     (( staging )) && detail+="$(as_colour $staging_col "+$staging")"
     (( stashed )) && detail+="$(as_colour $stashed_col "!$stashed")"
     [[ -n "$detail" ]] && detail=" $detail"
 
-    echo " $(in_section $BRANCH$detail)"
+    echo " $(wrap_in_section $BRANCH$detail)"
 }
 
 function update_hook() {
     BRANCH=$(git symbolic-ref --short HEAD 2> /dev/null)
-    # Overwrite prompt. Trailing space for command input.
-    PROMPT="$(in_section "%1d")$(branch_metadata) "
+    PROMPT="$(wrap_in_section "%1d")$(branch_metadata) "
 }
 
 autoload -Uz add-zsh-hook && add-zsh-hook precmd update_hook
@@ -56,7 +52,6 @@ autoload -Uz add-zsh-hook && add-zsh-hook precmd update_hook
 # Keybindings
 ##############
 
-# Rotate through the suggested auto-fill commands by recency.
 bindkey "^[f" history-beginning-search-forward  # opt+left
 bindkey "^[b" history-beginning-search-backward # opt+right
 
@@ -70,5 +65,9 @@ zle -N print_branch && bindkey "^B" print_branch # ctrl+b
 ##############
 
 source $HOME/.cargo/env
+
 source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# NOTE: Taken from a generated ".zprofile" file.
+source ~/.orbstack/shell/init.zsh 2>/dev/null
